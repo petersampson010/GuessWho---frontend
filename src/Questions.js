8eebc8164a0d1a7933d3a1e99d66c342c478218d
bogomilam <bogomilmihaylov@live.com>
2020-03-04 16:12:35 +0000

    enlarged all the modals questions to a visible size

--- src/Questions.js
+++ src/Questions.js
@@ -8,15 +8,15 @@ export default class Questions extends React.Component {
             <div style={this.props.et === null ? {display: "none"} : null} className='questions'>
                 SELECT A CATEGORY FOR YOUR QUESTION
                 <br></br>
-                <button onClick={(e) => this.props.attributeClick("colour")}>COLOUR</button>
-                <button onClick={(e) => this.props.attributeClick("number_of_eyes")}>NUMBER OF EYES</button>
-                <button onClick={(e) => this.props.attributeClick("hat")}>HAT</button>
-                <button onClick={(e) => this.props.attributeClick("ears")}>EARS</button>
-                <button onClick={(e) => this.props.attributeClick("horns")}>HORNS</button>
-                <button onClick={(e) => this.props.attributeClick("hair")}>HAIR</button>
-                <button onClick={(e) => this.props.attributeClick("fin")}>FIN</button>
-                <button onClick={(e) => this.props.attributeClick("nose")}>NOSE</button>
-                <button onClick={(e) => this.props.attributeClick("eyebrows")}>EYEBROWS</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("colour")}>COLOUR</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("number_of_eyes")}>NUMBER OF EYES</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("hat")}>HAT</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("ears")}>EARS</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("horns")}>HORNS</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("hair")}>HAIR</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("fin")}>FIN</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("nose")}>NOSE</button>
+                <button class="q-font" onClick={(e) => this.props.attributeClick("eyebrows")}>EYEBROWS</button>
                 <br></br>
                 <br></br>
                 <button className='glow-on-hover' onClick={this.props.takeAGuess}>TAKE A GUESS</button>
